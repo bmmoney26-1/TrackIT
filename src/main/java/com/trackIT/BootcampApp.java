1dab33a39581b9917fe9c452d1b3bb89b7dd7a27
bobbym.moneyjr <bmmoney261@gmail.com>
2026-03-28 13:35:56 -0400

    All 9 repositories wired up in Bootcamp service layer.

--- src/main/java/com/trackIT/BootcampApp.java
+++ src/main/java/com/trackIT/BootcampApp.java
@@ -1,10 +1,37 @@
 package com.trackIT;
 
+import com.trackIT.model.Student;
+import com.trackIT.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BootcampApp {
+@Autowired
+private StudentRepository studentRepository;
 
+@Autowired
+    private BlockerRepository blockerRepository;
 
+@Autowired
+    private AssignmentRepository assignmentRepository;
+
+@Autowired
+    private CertAssignmentRepository certAssignmentRepository;
+
+@Autowired
+    private CodingAssignmentRepository codingAssignmentRepository;
+
+@Autowired
+    private LearningSessionRepository learningSessionRepository;
+
+@Autowired
+    private NoteRepository noteRepository;
+
+@Autowired
+    private ProgressReportRepository progressReportRepository;
+
+@Autowired
+    private StudentRosterRepository studentRosterRepository;
 
 }
--- src/main/java/com/trackIT/repository/AssignmentRepository.java
+++ src/main/java/com/trackIT/repository/AssignmentRepository.java
@@ -1,8 +1,10 @@
 package com.trackIT.repository;
 import com.trackIT.model.Assignment;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AssignmentRepository extends JpaRepository<Assignment,Long> {
+
 }
