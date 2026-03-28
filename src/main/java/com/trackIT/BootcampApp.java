464eb27dba73d206aac705ab3ac19c7339a370ed
bobbym.moneyjr <bmmoney261@gmail.com>
2026-03-28 15:47:21 -0400

    Added wat to find a student by ID.

--- src/main/java/com/trackIT/BootcampApp.java
+++ src/main/java/com/trackIT/BootcampApp.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BootcampApp {
@@ -45,5 +46,7 @@ public List<Student> getAllStudents(){
  return studentRepository.findAll();
 }
 
-
+public Optional<Student> getStudentById(Long id){
+    return studentRepository.findById(id);
+}
 }
