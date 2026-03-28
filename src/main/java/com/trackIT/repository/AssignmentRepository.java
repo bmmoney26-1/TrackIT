4a17a0530ba8e7b75969bec930ee01f87e8df683
bobbym.moneyjr <bmmoney261@gmail.com>
2026-03-28 12:29:34 -0400

    Repositories made. Need to fill them.

---
+++ src/main/java/com/trackIT/repository/AssignmentRepository.java
@@ -0,0 +1,4 @@
+package com.trackIT.repository;
+
+public interface AssignmentRepository {
+}
--- src/main/java/com/trackIT/repository/StudentRepository.java
+++ src/main/java/com/trackIT/repository/StudentRepository.java
@@ -1,4 +1,13 @@
 package com.trackIT.repository;
+import com.trackIT.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface StudentRepository {
+@Repository
+public interface StudentRepository extends JpaRepository <Student, Long> {
+
+
+
+
+
 }
