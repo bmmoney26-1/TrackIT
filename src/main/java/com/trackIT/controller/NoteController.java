0bf9b34b8e42c260db98d94a2e859f8ece8ba9a8
bobbym.moneyjr <bmmoney261@gmail.com>
2026-04-01 11:46:40 -0400

    Added Notecontroller with full CRUD implementation. Future advanced queries planned

--- src/main/java/com/trackIT/controller/NoteController.java
+++ src/main/java/com/trackIT/controller/NoteController.java
@@ -1,4 +1,38 @@
 package com.trackIT.controller;
 
+import com.trackIT.BootcampApp;
+import com.trackIT.model.Note;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/notes")
 public class NoteController {
+    @Autowired
+    private BootcampApp bootcampApp;
+
+    @PostMapping
+    public Note addNote(@RequestBody Note note) {
+
+        return bootcampApp.createNote(note);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Note> noteId(@PathVariable Long id) {
+        return bootcampApp.getNoteById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void noteDeleteById(@PathVariable Long id) {
+        bootcampApp.deleteNote(id);
+    }
+
+    @PutMapping("/{id}")
+    public Note updateNote(@RequestBody Note note, @PathVariable Long id) {
+        return bootcampApp.updateNoteInfo(note);
+    }
 }
+
