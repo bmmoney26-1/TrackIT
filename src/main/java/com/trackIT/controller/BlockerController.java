d84dbaa4a88cb5beb7a5e868750c0197c09f5b70
bobbym.moneyjr <bmmoney261@gmail.com>
2026-04-01 10:28:57 -0400

    Added BLocker controller with full CRUD implementation

--- src/main/java/com/trackIT/controller/BlockerController.java
+++ src/main/java/com/trackIT/controller/BlockerController.java
@@ -2,9 +2,13 @@ package com.trackIT.controller;
 
 
 import com.trackIT.BootcampApp;
+import com.trackIT.model.Blocker;
+import com.trackIT.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/blockers")
@@ -12,6 +16,25 @@ public class BlockerController {
     @Autowired
     private BootcampApp bootcampApp;
 
+    @PostMapping("/{id}")
+    public Blocker addBlocker(@RequestBody Blocker blocker, @PathVariable Long id){
+
+        return bootcampApp.createBlocker(blocker,id);
+    }
+    @GetMapping("/{id}")
+    public List<Blocker> blockerId(@PathVariable Long id){
+        return bootcampApp.getStudentBlockers(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void blockerDeleteById (@PathVariable Long id){
+        bootcampApp.deleteBlocker(id);
+    }
+
+    @PutMapping("/{id}")
+    public Blocker updateBlocker(@RequestBody Blocker blocker){
+        return bootcampApp.updateBlockerInfo(blocker);
+    }
 
 
 
